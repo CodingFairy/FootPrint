9b5c0663ccb0652918d2d402da40495166aa65b5
cuiods <cuihaoemail@163.com>
2017-03-20 17:54:24 +0800

    Impl of dao interface

--- src/main/java/com/codingfairy/data/daoImpl/StarDaoImpl.java
+++ src/main/java/com/codingfairy/data/daoImpl/StarDaoImpl.java
@@ -2,25 +2,32 @@ package com.codingfairy.data.daoImpl;
 
 import com.codingfairy.data.dao.StarDao;
 import com.codingfairy.data.entity.StarEntity;
+import com.codingfairy.data.repository.StarRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
 
 /**
  * Created by cuihao on 2017/3/20.
  */
 @Repository
 public class StarDaoImpl implements StarDao {
+
+    @Resource
+    private StarRepository starRepository;
+
     @Override
     public StarEntity save(StarEntity starEntity) {
-        return null;
+        return starRepository.save(starEntity);
     }
 
     @Override
     public StarEntity findById(int id) {
-        return null;
+        return starRepository.findOne(id);
     }
 
     @Override
     public void delete(int id) {
-
+        starRepository.delete(id);
     }
 }
--- src/main/java/com/codingfairy/data/daoImpl/StoryPictureDaoImpl.java
+++ src/main/java/com/codingfairy/data/daoImpl/StoryPictureDaoImpl.java
@@ -2,25 +2,32 @@ package com.codingfairy.data.daoImpl;
 
 import com.codingfairy.data.dao.StoryPictureDao;
 import com.codingfairy.data.entity.StoryPicEntity;
+import com.codingfairy.data.repository.StoryPicRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
 
 /**
  * Created by cuihao on 2017/3/20.
  */
 @Repository
 public class StoryPictureDaoImpl implements StoryPictureDao {
+
+    @Resource
+    private StoryPicRepository storyPicRepository;
+
     @Override
     public StoryPicEntity save(StoryPicEntity storyPicEntity) {
-        return null;
+        return storyPicRepository.save(storyPicEntity);
     }
 
     @Override
     public void delete(int id) {
-
+        storyPicRepository.delete(id);
     }
 
     @Override
     public StoryPicEntity findById(int id) {
-        return null;
+        return storyPicRepository.findOne(id);
     }
 }
