faf5f1f1e0f7305a80b2c3bb5899e1099f62a3f5
yyy2015 <2456700983@qq.com>
2017-03-20 17:59:17 +0800

    VOs

--- src/main/java/com/codingfairy/bl/vo/UserVo.java
+++ src/main/java/com/codingfairy/bl/vo/UserVo.java
@@ -7,19 +7,40 @@ import lombok.Data;
 import org.springframework.beans.BeanUtils;
 
 import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class UserVo {
     private int id;
     private String name;
+    private String password;
     private String avatar;
     private String phone;
     private Timestamp createdAt;
     private Timestamp updatedAt;
     private Integer valid;
+    private Timestamp deletedAt;
     private Gender gender;
+    private String description;
+    private List<UserVo> following;
+    private List<UserVo> followed;
+    private List<StoryVo> storyVos;
+    private List<CommentVo> commentVos;
+    private List<RouteVo> routeVos;
 
     public UserVo(UserEntity userEntity) {
+        this(userEntity,false);
+    }
+
+    public UserVo(UserEntity userEntity,boolean hasSession){
         BeanUtils.copyProperties(userEntity,this,"deletedAt","password");
+        if(hasSession){
+            following = userEntity.getFollowing().stream().map(UserVo::new).collect(Collectors.toList());
+            followed = userEntity.getFollower().stream().map(UserVo::new).collect(Collectors.toList());
+            storyVos = userEntity.getUserStories().stream().map(StoryVo::new).collect(Collectors.toList());
+            commentVos = userEntity.getCommentEntities().stream().map(CommentVo::new).collect(Collectors.toList());
+            routeVos = userEntity.getRouteEntities().stream().map(RouteVo::new).collect(Collectors.toList());
+        }
     }
 }
