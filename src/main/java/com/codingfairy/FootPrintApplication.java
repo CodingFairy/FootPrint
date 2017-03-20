c596df8e2324f68cc3ec4a877856cebcc446d405
cuiods <cuihaoemail@163.com>
2017-03-20 11:56:35 +0800

    Initial Project

---
+++ src/main/java/com/codingfairy/FootPrintApplication.java
@@ -0,0 +1,16 @@
+package com.codingfairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class of the application
+ * @author cuihao
+ */
+@SpringBootApplication
+public class FootPrintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootPrintApplication.class,args);
+    }
+}
---
+++ src/test/java/com/codingfairy/BaseTest.java
@@ -0,0 +1,8 @@
+package com.codingfairy;
+
+/**
+ * Before writing any test, please extend <code>BaseTest</code> first.
+ * @author cuihao
+ */
+public class BaseTest {
+}
