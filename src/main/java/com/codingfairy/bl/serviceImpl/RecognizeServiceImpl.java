5c3215d29e068b74f250f7d58f9e7c381f49c275
Harry1001 <benchaodong@gmail.com>
2017-03-21 14:54:08 +0800

    还缺一些dao接口

--- src/main/java/com/codingfairy/bl/serviceImpl/RecognizeServiceImpl.java
+++ src/main/java/com/codingfairy/bl/serviceImpl/RecognizeServiceImpl.java
@@ -2,60 +2,63 @@ package com.codingfairy.bl.serviceImpl;
 
 import com.codingfairy.bl.service.RecognizeService;
 import com.codingfairy.bl.vo.EmotionVo;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import com.codingfairy.web.config.EmotionConfig;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * Created by Mr.Zero on 2017/3/20.
  */
+@Service
 public class RecognizeServiceImpl implements RecognizeService {
     @Override
     public EmotionVo getImageEmotion(String imgUrl) {
-        String testUrl = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490605057&di=ee127846a5e30ca87ddec5fabc12740f&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.cmol.com%2Fimages%2Fuploadfile%2F2015%2F0728%2F20150728012521632.jpg";
-        HttpClient httpClient = new DefaultHttpClient();
+        EmotionConfig config = new EmotionConfig();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("url", imgUrl);
         try {
-            URIBuilder uriBuilder = new URIBuilder("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
-            URI uri = uriBuilder.build();
-            HttpPost request = new HttpPost(uri);
-
-            // Request headers. Replace the example key below with your valid subscription key.
-            request.setHeader("Content-Type", "application/json");
-            request.setHeader("Ocp-Apim-Subscription-Key", "aa04808f99754e89b42bb72da5965dfc");
-
-            // Request body. Replace the example URL below with the URL of the image you want to analyze.
-            StringEntity reqEntity = new StringEntity("{ \"url\": \""+testUrl+"\" }");
-            request.setEntity(reqEntity);
-
-            HttpResponse response = httpClient.execute(request);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                System.out.println("=================================================");
-                System.out.println(EntityUtils.toString(entity));
-                System.out.println("=================================================");
+            HttpResponse<JsonNode> jsonResponse = Unirest.post(config.getApi())
+                    .header("Content-Type", "application/json")
+                    .header("Ocp-Apim-Subscription-Key", config.getToken1())
+                    .body(jsonObject)
+                    .asJson();
+            JSONArray jsonArray = jsonResponse.getBody().getArray();
+            if (jsonArray.length()==0){
+                return null;
+            } else {
+                double anger = 0.0;
+                double contempt = 0.0;
+                double disgust = 0.0;
+                double fear = 0.0;
+                double happiness = 0.0;
+                double neutral = 0.0;
+                double sadness = 0.0;
+                double surprise = 0.0;
+                int count = 0;
+                for (int i = 0 ; i<jsonArray.length();i++){
+                    JSONObject scores = jsonArray.getJSONObject(i).getJSONObject("scores");
+                    anger+=scores.getDouble("anger");
+                    contempt+=scores.getDouble("contempt");
+                    disgust+=scores.getDouble("disgust");
+                    fear+=scores.getDouble("fear");
+                    happiness+=scores.getDouble("happiness");
+                    neutral+=scores.getDouble("neutral");
+                    sadness+=scores.getDouble("sadness");
+                    surprise+=scores.getDouble("surprise");
+                    count++;
+                }
+                return new EmotionVo(anger/count,contempt/count,disgust/count,fear/count,
+                        happiness/count,neutral/count,sadness/count,surprise/count);
             }
-
-        } catch (URISyntaxException e) {
+        } catch (UnirestException e) {
             e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            return null;
         }
-        return new EmotionVo();
     }
 }
--- src/main/java/com/codingfairy/bl/serviceImpl/StoryServiceImpl.java
+++ src/main/java/com/codingfairy/bl/serviceImpl/StoryServiceImpl.java
@@ -1,14 +1,13 @@
 package com.codingfairy.bl.serviceImpl;
 
+import com.codingfairy.bl.service.RecognizeService;
 import com.codingfairy.bl.service.StoryService;
 import com.codingfairy.bl.vo.*;
+import com.codingfairy.data.dao.CommentDao;
 import com.codingfairy.data.dao.StarDao;
 import com.codingfairy.data.dao.StoryDao;
 import com.codingfairy.data.dao.UserDao;
-import com.codingfairy.data.entity.StarEntity;
-import com.codingfairy.data.entity.StoryEntity;
-import com.codingfairy.data.entity.StoryPicEntity;
-import com.codingfairy.data.entity.UserEntity;
+import com.codingfairy.data.entity.*;
 import com.codingfairy.util.constant.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,12 +25,16 @@ public class StoryServiceImpl implements StoryService {
     private UserDao userDao;
     private StoryDao storyDao;
     private StarDao starDao;
+    private CommentDao commentDao;
+    private RecognizeService recognizeService;
 
     @Autowired
-    public StoryServiceImpl(UserDao userDao, StoryDao storyDao, StarDao starDao) {
+    public StoryServiceImpl(UserDao userDao, StoryDao storyDao, StarDao starDao, CommentDao commentDao, RecognizeService recognizeService) {
         this.userDao = userDao;
         this.storyDao = storyDao;
         this.starDao = starDao;
+        this.commentDao = commentDao;
+        this.recognizeService = recognizeService;
     }
 
     @Override
@@ -99,42 +102,111 @@ public class StoryServiceImpl implements StoryService {
     }
 
     @Override
-    public ResultVo<Void> unStarStory(int userId, int storyId) {
-        //TODO no dao
-        return null;
+    public ResultVo<Void> unStarStory(int starId) {
+        starDao.delete(starId);
+        return new ResultVo<>(ErrorCode.SUCCESS, null);
     }
 
     @Override
+    @Transactional
     public ResultVo<CommentVo> comment(int userId, int storyId, String content, int parent) {
-        return null;
+        UserEntity userEntity = userDao.findUserById(userId);
+        if (userEntity == null) {
+            return new ResultVo<>(ErrorCode.USER_NOT_FOUND, null);
+        }
+        StoryEntity storyEntity = storyDao.findById(storyId);
+        if (storyEntity == null){
+            return new ResultVo<>(ErrorCode.STORY_NOT_FOUND, null);
+        }
+        CommentEntity entity = new CommentEntity();
+        entity.setUserEntity(userEntity);
+        entity.setStoryEntity(storyEntity);
+        //<0表示这个评论不存在父评论s
+        if (userId<0){
+            entity.setParentComment(null);
+        } else {
+            entity.setParentComment(commentDao.findById(parent));
+        }
+        entity.setContent(content);
+        CommentEntity savedEntity = commentDao.save(entity);
+        if (savedEntity == null){
+            return new ResultVo<>(ErrorCode.SQL_ERROR, null);
+        }
+        return new ResultVo<>(ErrorCode.SUCCESS, new CommentVo(savedEntity, true));
     }
 
     @Override
     public ResultVo<Void> deleteComment(int commentId) {
-        return null;
+        commentDao.delete(commentId);
+        return new ResultVo<>(ErrorCode.SUCCESS, null);
     }
 
     @Override
     public List<CommentVo> getCommentList(int storyId) {
+        //todo no dao
         return null;
     }
 
     @Override
     public List<StoryVo> getStoryList(double longitude, double latitude, int num) {
-        return null;
+        List<StoryEntity> entityList = storyDao.findByLocation(longitude, latitude, num);
+        List<StoryVo> voList = new ArrayList<>();
+        for (StoryEntity po : entityList) {
+            voList.add(new StoryVo(po));
+        }
+        return voList;
     }
 
     @Override
     public List<RouteVo> getRouteList(int userId) {
+        //todo no dao
         return null;
     }
 
     @Override
     public List<StoryVo> getRecommendStory(double longitude, double latitude) {
+        //TODO recommend ???
         return null;
     }
 
     private void analyzeEmotion(int storyId, List<String> imageUrlList){
-
+        Thread thread = new Thread(() -> {
+            double anger = 0.0;
+            double contempt = 0.0;
+            double disgust = 0.0;
+            double fear = 0.0;
+            double happiness = 0.0;
+            double neutral = 0.0;
+            double sadness = 0.0;
+            double surprise = 0.0;
+            int count = 0;
+            for (String imgUrl : imageUrlList){
+                EmotionVo emotionVo = recognizeService.getImageEmotion(imgUrl);
+                if (emotionVo!=null){
+                    anger+=emotionVo.getAnger();
+                    contempt+=emotionVo.getContempt();
+                    disgust+=emotionVo.getDisgust();
+                    fear+=emotionVo.getFear();
+                    happiness+=emotionVo.getHappiness();
+                    neutral+=emotionVo.getNeutral();
+                    sadness+=emotionVo.getSadness();
+                    surprise+=emotionVo.getSurprise();
+                    count++;
+                }
+            }
+            StoryEntity storyEntity = storyDao.findById(storyId);
+            if (storyEntity!=null){
+                storyEntity.setAnger(anger/count);
+                storyEntity.setContempt(contempt/count);
+                storyEntity.setDisgust(disgust/count);
+                storyEntity.setFear(fear/count);
+                storyEntity.setHappiness(happiness/count);
+                storyEntity.setNeutral(neutral/count);
+                storyEntity.setSadness(sadness/count);
+                storyEntity.setSurprise(surprise/count);
+                storyDao.save(storyEntity);
+            }
+        });
+        thread.start();
     }
 }
